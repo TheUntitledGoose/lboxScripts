01d0a0e7b992239322f1eb582a99016103df8271
TheUntitledGoose <75637597+TheUntitledGoose@users.noreply.github.com>
2022-03-31 19:03:40 -0700

    Made it look better

--- modules.lua
+++ modules.lua
@@ -16,13 +16,18 @@ local function mydraw()
     local aimbot = gui.GetValue("aim bot")
     local aimbotMethod = gui.GetValue("aim method")
     local dt = gui.GetValue("double tap")
+    local aa = gui.GetValue("anti aim")
 
     if dt == 1 then dt = "on" else dt = "off" end
     if aimbot == 1 then aimbot = "on" else aimbot = "off" end
+    if aa == 1 then aa = "on" else aa = "off" end
 
     -- draw a box around the text
-    draw.Color(255, 255, 255, 25)
-    draw.FilledRect((middleScreenX/5)*3.25 - 40, (middleScreenY/5)*2 - 25, (middleScreenX/5)*3.25 + textW + 50, (middleScreenY/5)*2 + textH + 150)
+    draw.Color(24, 9, 14, 75)
+    draw.FilledRect((middleScreenX/5)*3.25 - 40, (middleScreenY/5)*2 - 5, (middleScreenX/5)*3.25 + textW + 50, (middleScreenY/5)*2 + textH + 5)
+    draw.Color(94, 61, 77, 255)
+    draw.Line((middleScreenX/5)*3.25 - 40, (middleScreenY/5)*2 - 5, (middleScreenX/5)*3.25 + textW + 50, (middleScreenY/5)*2 - 5)
+    draw.Color(255, 255, 255, 255)
 
     local boxLeftOffset = (middleScreenX/5)*3.25 - 40
     local boxRightOffset = (middleScreenX/5)*3.25 + textW + 50
@@ -32,13 +37,14 @@ local function mydraw()
     draw.Color(255, 255, 255, 255)
     draw.Text((middleScreenX/5)*3.25 + 10, (middleScreenY/5)*2, "Modules")
 
-    draw.Text(boxInBetween + boxLeftOffset - math.floor(draw.GetTextSize("DT: " .. tostring(dt))/2),      (middleScreenY/5)*2 + textH + 10, "DT: " .. dt)
-    draw.Text(boxInBetween + boxLeftOffset - math.floor(draw.GetTextSize("Aim: " .. tostring(aimbot))/2), (middleScreenY/5)*2 + textH + 30, "Aim: " .. aimbot)
-    draw.Text(boxInBetween + boxLeftOffset - math.floor(draw.GetTextSize("Aim method: ")/2),              (middleScreenY/5)*2 + textH + 50, "Aim method: ")
-    draw.Text(boxInBetween + boxLeftOffset - math.floor(draw.GetTextSize(tostring(aimbotMethod))/2),      (middleScreenY/5)*2 + textH + 70, aimbotMethod)
-
-    
-
+    draw.Text(10 + boxLeftOffset, (middleScreenY/5)*2 + textH + 10, "DT: ")
+    draw.Text(10 + boxLeftOffset, (middleScreenY/5)*2 + textH + 30, "Aim: ")
+    draw.Text(10 + boxLeftOffset, (middleScreenY/5)*2 + textH + 50, "AA: ")
+    draw.Text(10 + boxLeftOffset, (middleScreenY/5)*2 + textH + 70, "Aim method: ")
+    draw.Text(10 + boxLeftOffset, (middleScreenY/5)*2 + textH + 90, aimbotMethod)
+    draw.Text(boxRightOffset - 40, (middleScreenY/5)*2 + textH + 10, "[ " .. dt .. " ]")
+    draw.Text(boxRightOffset - 40, (middleScreenY/5)*2 + textH + 30, "[ " .. aimbot .. " ]")
+    draw.Text(boxRightOffset - 40, (middleScreenY/5)*2 + textH + 50, "[ " .. aa .. " ]")
 
 end
 
